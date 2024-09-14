83804e530b81ae442c544d8358ef6320a66d230f
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2024-09-14 00:11:58 -0700

    Update madden25.py

--- madden25.py
+++ madden25.py
@@ -17,8 +17,8 @@ black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
-blue = pygame.Color(0, 0, 255)
-yellow = pygame.Color(255, 255, 0)
+blue = pygame.Color(0, 71, 171)
+yellow = pygame.Color(255, 215, 0)
 
 pygame.init()
 
